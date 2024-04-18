b05f6781c3d6f178fc0070c7b136312b6ae19874
GeremyColley <78615407+GeremyColley@users.noreply.github.com>
2024-04-18 22:50:52 +0200

    Second commit

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
+import { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View, ScrollView } from 'react-native';
+import { Ionicons, AntDesign } from '@expo/vector-icons';
 
 export default function App() {
+
+  const [email, setEmail] = useState("");
+  const [description, setDescription] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  let RTY = false;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Ionicons name="md-checkmark-circle" size={32} color="green" />  
+      <AntDesign name="carryout" size={24} color="black" />
+      <TextInput
+        style={{ height: 44, borderColor: "gray", borderWidth: 1, marginBottom: 10 }}
+        onChangeText={text => {
+          setEmail(text);
+        }}
+        value={email}
+      />
+
+      <TextInput
+        style={{height: 100, borderColor: "gray", borderWidth: 1, marginBottom: 10}}
+        placeholder="Description"
+        multiline={true}
+        textAlignVertical="top"
+        onChangeText={(text) => {
+          setDescription(text);
+        }}
+        value={description}
+      />
+
+      <TextInput
+        style={{height: 44, borderColor: "gray", borderWidth: 1, marginBottom: 10}}
+        placeholder="Phone"
+        keyboardType="numeric"
+        onChangeText={(text) => {
+          setPhone(text);
+        }}
+        value={phone}
+      />
+
+      <TextInput
+        style={{height: 44, borderColor: "gray", borderWidth: 1}}
+        placeholder="Password"
+        secureTextEntry={true}
+        onChangeText={(text) => {
+          setPassword(text);
+        }}
+        value={password}
+      />
+      
     </View>
   );
 }
@@ -18,3 +66,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+/*
+<View style={styles.container}>
+      <Text>Alice</Text>
+      <Text>Ma fille</Text>
+      <StatusBar style="auto" />
+    </View>
+
+
+*/
+
+/*
+ <ScrollView>
+      <View style={{ height: 400, backgroundColor: "crimson" }}></View>
+      <View style={{ height: 400, backgroundColor: "pink" }}></View>
+      <View style={{ height: 400, backgroundColor: "salmon" }}></View>
+      <View style={{ height: 400, backgroundColor: "white" }}></View>
+      <View style={{ height: 400, backgroundColor: "deeppink" }}></View>
+    </ScrollView>
+
+*/
